70ad3df685e642007fb3b2e34d983f2c902d5d90
Alex <alex.rodrigues23997@gmail.com>
2022-07-20 22:48:03 -0300

    Projeto criado

---
+++ src/main/java/com/azold6/xlsxspringchallenge/XlsxSpringChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.azold6.xlsxspringchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XlsxSpringChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XlsxSpringChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/azold6/xlsxspringchallenge/XlsxSpringChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azold6.xlsxspringchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XlsxSpringChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
